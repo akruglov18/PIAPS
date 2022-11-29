45d93d41c1ab3dc8c6f21029a4f6928cb1c98712
IvanovAA-bin <ivanov-arkadij1@mail.ru>
2022-11-29 18:34:16 +0300

    Changename build fix

--- PIAPS_ServerCore/src/main/java/com/unn/piap_serverside/net_protocol/NP_RegistrationPacket.java
+++ PIAPS_ServerCore/src/main/java/com/unn/piap_serverside/net_protocol/NP_RegistrationPacket.java
@@ -1,42 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.unn.piap_serverside.net_protocol;
 
 import com.google.gson.Gson;
+import lombok.AllArgsConstructor;
 
-/**
- *
- * @author STALKER
- */
-public class NP_RegistrationRequestPacket implements NetPackage.NetMessageInterface {
+@AllArgsConstructor
+public class NP_RegistrationPacket implements NetPackage.NetMessageInterface {
+    public boolean isRequest;
+    
+    // REQUEST
     public String login;
     public String password;
+    public String fio;
+    public NP_RegistrationPacket.USER_TYPE userType;
     
-    public NP_RegistrationRequestPacket(String login, String password) {
-        this.login = login;
-        this.password = password;
+    // RESPONSE
+    public NP_RegistrationPacket.RESPONSE_TYPE respType;
+    
+    
+    public enum USER_TYPE {
+        COORDINATOR,
+        CUSTOMER
     }
     
-    public NP_RegistrationRequestPacket(NP_RegistrationRequestPacket np_rrp) {
-        this.login = np_rrp.login;
-        this.password = np_rrp.password;
+    public enum RESPONSE_TYPE {
+        REGISTERED,
+        ERROR_LOGIN_ALREADY_EXISTS,
+        ERROR_INTERNAL_SERVER_ERROR
     }
     
-    public static boolean isCorrect(NP_RegistrationRequestPacket regReqPc) {
-        if (regReqPc == null)
+    public static boolean isCorrect(NP_RegistrationPacket rp) {
+        if (rp == null)
             return false;
-        if (regReqPc.login == null)
-            return false;
-        if (regReqPc.password == null)
-            return false;
+        if (rp.isRequest) {
+            if (rp.login == null || rp.password == null ||
+                    rp.fio == null || rp.userType == null)
+                return false;
+        } else {
+            if (rp.respType == null)
+                return false;
+        }
         return true;
     }
 
     @Override
     public NetPackage.NET_MSG_TYPE getMessageType() {
-        return NetPackage.NET_MSG_TYPE.REQUEST;
+        if (isRequest)
+            return NetPackage.NET_MSG_TYPE.REQUEST;
+        else
+            return NetPackage.NET_MSG_TYPE.RESPONSE;
     }
 
     @Override
